f7b33487e499e4ec0ba03459b5a148a154289dbb
Bandss <foreignbandss@hotmail.com>
2026-03-02 18:44:39 -0500

    Initialize QApplication and BrowserWindow in main

--- cannnddyy/main.cpp
+++ cannnddyy/main.cpp
@@ -1,1 +1,13 @@
+#include <QApplication>
+#include "browserwindow.h"
 
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    BrowserWindow window;
+    window.resize(1200, 800);
+    window.show();
+
+    return app.exec();
+}
