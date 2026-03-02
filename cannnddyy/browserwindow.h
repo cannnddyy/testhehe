e42a316a4d5c8295746f8d342860357f97f24f80
Bandss <foreignbandss@hotmail.com>
2026-03-02 18:44:57 -0500

    Add BrowserWindow class with basic UI setup

--- cannnddyy/browserwindow.h
+++ cannnddyy/browserwindow.h
@@ -1,1 +1,24 @@
+#pragma once
 
+#include <QMainWindow>
+#include <QTabWidget>
+#include <QLineEdit>
+#include <QWebEngineView>
+#include <QWebEngineUrlSchemeHandler>
+
+class BrowserWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    BrowserWindow();
+
+private:
+    QTabWidget *tabs;
+    QLineEdit *urlBar;
+
+    void createNewTab(const QUrl &url = QUrl("https://www.bing.com"));
+    void setupUI();
+    void setupShortcuts();
+    void handleCustomUrl(QWebEngineView *view);
+};
