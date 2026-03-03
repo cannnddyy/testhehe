254f12e05f5df1bcf5b69d0666a17f48271547e3
Bandss <foreignbandss@hotmail.com>
2026-03-02 20:22:38 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -e
 
-# 1. install dependencies if not already
+# optional: install dependencies
 sudo apt update
 sudo apt install -y build-essential cmake qt6-base-dev qt6-webengine-dev qt6-webchannel-dev libgl1-mesa-dev
 
-# 2. remove old build
+# remove old build
 rm -rf build
-
-# 3. make build folder
 mkdir build
 cd build
 
-# 4. generate makefiles and build
-cmake ..
+# run cmake inside cannnddyy folder
+cmake ../cannnddyy
 make -j$(nproc)
 
-# 5. run the browser
+# run the browser
 ./cannnddyy
