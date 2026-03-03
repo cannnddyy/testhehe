1467bf5524ec41bf178dc7e976029dfa660c6332
Bandss <foreignbandss@hotmail.com>
2026-03-02 19:00:09 -0500

    Refactor BrowserWindow header with new includes and members

--- cannnddyy/browserwindow.h
+++ cannnddyy/browserwindow.h
@@ -1,9 +1,16 @@
-#pragma once
+#ifndef BROWSERWINDOW_H
+#define BROWSERWINDOW_H
 
 #include <QMainWindow>
 #include <QTabWidget>
 #include <QLineEdit>
-#include <QWebEngineView>
+#include <QToolBar>
+#include <QComboBox>
+#include <QVector>
+#include <QPair>
+#include <QTime>
+#include <QSettings>
+#include <QtWebEngineWidgets/QWebEngineView>
 
 class BrowserWindow : public QMainWindow
 {
@@ -12,10 +19,22 @@ class BrowserWindow : public QMainWindow
 public:
     BrowserWindow();
 
+    QVector<QPair<QString, QString>> historyList;
+
 private:
     QTabWidget *tabs;
     QLineEdit *addressBar;
+    QComboBox *engineSelector;
+
+    QString currentSearchEngine;
+    QStringList blockedSites;
+
+    QTime sessionStart;
+    int allowedMinutes = 60;
 
     void setupUI();
-    void addNewTab(const QUrl &url);
+    void addNewTab(QUrl url = QUrl("candy://about"));
+    void recordHistory(const QUrl &url, const QString &title);
 };
+
+#endif
