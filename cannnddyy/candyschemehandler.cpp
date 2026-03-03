599d5c888da3b9b746a7b230e2ae3b5b09ff6fc9
Bandss <foreignbandss@hotmail.com>
2026-03-02 19:01:20 -0500

    Refactor CandySchemeHandler::requestStarted method
    Refactor requestStarted to simplify file path handling and improve error response.

--- cannnddyy/candyschemehandler.cpp
+++ cannnddyy/candyschemehandler.cpp
@@ -1,26 +1,26 @@
 #include "candyschemehandler.h"
 #include <QFile>
 
-void CandySchemeHandler::requestStarted(QWebEngineUrlRequestJob *job)
+CandySchemeHandler::CandySchemeHandler(QObject *parent)
+    : QWebEngineUrlSchemeHandler(parent)
+{
+}
+
+void CandySchemeHandler::requestStarted(
+    QWebEngineUrlRequestJob *job)
 {
     QString path = job->requestUrl().path();
-    QString filePath;
 
-    if (path == "/about")
-        filePath = ":/pages/about.html";
-    else if (path == "/history")
-        filePath = ":/pages/history.html";
-    else if (path == "/settings")
-        filePath = ":/pages/settings.html";
-    else
-        filePath = ":/pages/about.html";
+    QString filePath = ":/pages" + path + ".html";
 
     QFile *file = new QFile(filePath);
 
-    if (!file->open(QIODevice::ReadOnly)) {
-        job->fail(QWebEngineUrlRequestJob::UrlNotFound);
-        return;
+    if(file->open(QIODevice::ReadOnly))
+        job->reply("text/html", file);
+    else
+    {
+        QByteArray *data =
+        new QByteArray("<h1>Page Not Found</h1>");
+        job->reply("text/html", data);
     }
-
-    job->reply("text/html", file);
 }
